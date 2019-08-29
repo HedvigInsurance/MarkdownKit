c4f4542fcfb9f728057ef0a2006e9d54716c3990
Sam Pettersson <sam@sampettersson.com>
2019-08-29 14:43:46 +0200

    Add swift package support

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MarkdownKit",
+    products: [
+        .library(
+            name: "MarkdownKit",
+            targets: ["MarkdownKit"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "MarkdownKit",
+            dependencies: [],
+            path: "MarkdownKit",
+            exclude: [
+                "Sources/AppKit/**/*"
+            ]),
+    ]
+)
